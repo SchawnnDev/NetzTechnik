9260491df031c5e8cb6cdfe2edbe0691a31e6454
= <iperskill@gmail.com>
2019-10-19 12:06:43 +0200

    continue client

--- Hamming/Client/client.rb
+++ Hamming/Client/client.rb
@@ -1,21 +1,56 @@
+def get_parity_bits(length)
+	parity_bits = []
+	curr_val = 0
+	
+	until 2 ** curr_val > length
+		parity_bits << 2 ** curr_val
+		curr_val += 1
+	end
+	
+	return parity_bits
+	
+end
+
+def get_positions(pos, length)
+	positions = []
+	curr_val = pos
+	
+	until curr_val > length
+	
+		for a in 1..pos do
+			positions << curr_val
+			curr_val += 1
+		end
+		
+		curr_val += pos
+
+	end
+	
+	return positions
+
+end
+
+def is_even(bit_string, where)
+	count = 0
+
+	where.each { |x| count += bit_string[x - 1].to_i }
+	
+	return count % 2 == 0
+
+end
+
 puts "Please the byte of data you want to encode (8 bits)"
-to_encode = gets.chomp
+to_encode = "10011010"#gets.chomp
 
 while to_encode.length != 8
 puts "Incorrect byte of data length"
 to_encode = gets.chomp
 end
 
-e_length = to_encode.length
-curr_val = 0
-
-until 2 ** curr_val > e_length
-
-puts 2 ** curr_val
-to_encode.insert(2 ** curr_val - 1, '_')
-
-curr_val += 1
-
-end
+get_parity_bits(to_encode.length).each { |x|
+	even = is_even(to_encode, get_positions(x, to_encode.length))
+	puts "x " << String(x) << " is " << (even ? "true" : "false")
+	to_encode.insert(x - 1, even ? "0" : "1")
+}
 
 puts "Encodierter Hamming-Code: " << to_encode
