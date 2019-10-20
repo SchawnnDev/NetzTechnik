12d63de2bf7e77ad5531156bb63d738ebce58984
= <iperskill@gmail.com>
2019-10-20 16:46:39 +0200

    server fix

--- Hamming/Server/Program.cs
+++ Hamming/Server/Program.cs
@@ -22,20 +22,13 @@ namespace Server
 			}
 
 			var array = input.ToCharArray();
-			var output = 0;
-			foreach (var parity in GetParity(OutputLength + 1))
-			{
-				int sum = 0;
-				foreach (var position in GetPositions(parity, InputLength)) sum += int.Parse(array[position - 1].ToString());
-
-				output += sum * parity;
-			}
+			var output = GetParity(OutputLength).Sum(parity => Mod(array, GetPositions(parity, InputLength)) * parity);
 
 			if (output != 0)
 			{
 				var fixChar = FixChar(input[output - 1]);
 
-				Console.WriteLine($"Byte at position {output} is incorrect. Fixed from { FixChar(fixChar) } to { fixChar } ");
+				Console.WriteLine($"Byte at position {output} from {DeleteParity(array)} is incorrect. Fixed from { FixChar(fixChar) } to { fixChar } ");
 
 				array[output - 1] = fixChar;
 
@@ -44,6 +37,8 @@ namespace Server
 			Console.WriteLine($"Decoded Hamming-Code: {DeleteParity(array)}");
 
 		}
+
+		private static int Mod(char[] array, int[] where) => @where.Aggregate(0, (current, i) => current + array[i - 1]) % 2;
 
 		private static string DeleteParity(char[] array)
 		{
@@ -78,16 +73,16 @@ namespace Server
 			return list.ToArray();
 		}
 
-		private static IEnumerable<int> GetPositions(int pos, int length)
+		private static int[] GetPositions(int pos, int length)
 		{
 			var list = new List<int>();
 			var currentVal = pos;
 			while (currentVal < length)
 			{
 				for (var i = 1; i <= pos; i++)
-					currentVal = Math.Min(currentVal + 1, length);
+					list.Add(currentVal++);
 
-				currentVal = Math.Min(currentVal + pos, length);
+				currentVal += pos;
 
 			}
 
